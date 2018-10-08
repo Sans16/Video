720787bdf4c787e21397594e15d7ba69d54dcce0
Sanusi Sheriff <sanusisheriff16@gmail.com>
2018-10-08 08:01:59 -0800

    Features added

--- app/src/main/java/com/example/ssanusi/cameraandgallery/MainActivity.java
+++ app/src/main/java/com/example/ssanusi/cameraandgallery/MainActivity.java
@@ -1,13 +1,216 @@
 package com.example.ssanusi.cameraandgallery;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.pickCameraImage)
+    ImageButton pickCameraImage;
+    @BindView(R.id.pickGalleryImage)
+    ImageButton pickGalleryImage;
+    @BindView(R.id.acceptImage)
+    ImageButton acceptImage;
+    @BindView(R.id.profile_image)
+    ImageView profile_image;
+
+
+    private Unbinder unbinder;
+    private static final int REQUEST_EXTERNAL_STORAGE_PERMISSIONS = 1234;
+    private static final int IMAGE_GALLERY_REQUEST = 1;
+    private static final int IMAGE_CAMERA_REQUEST = 2;
+    private int choosenAction;
+    private File photoFile = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        unbinder = ButterKnife.bind(this);
+        pickCm();
+    }
+
+
+    public void pickCm(){
+        pickCameraImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                photoFile = null;
+                choosenAction = 0;
+                boolean result = checkStoragePermission(MainActivity.this);
+                if (result) photoCameraIntent();
+            }
+        });
+        pickGalleryImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                photoFile = null;
+                choosenAction = 1;
+                boolean result = checkStoragePermission(MainActivity.this);
+                if (result) photoGalleryIntent();
+            }
+        });
+        acceptImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (photoFile != null) {
+                    Glide.with(getBaseContext()).load(photoFile)
+                            .apply(new RequestOptions().error(R.drawable.icons8).placeholder(R.drawable.icons8).fitCenter())
+                            .into(profile_image);
+                }
+            }
+        });
+    }
+
+    private void onCaptureImageResult(Intent data) {
+        // uri = data.getData();
+        Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        thumbnail.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+
+        photoFile = new File(Environment.getExternalStorageDirectory(), System.currentTimeMillis() + ".jpg");
+        FileOutputStream fo;
+        try {
+            photoFile.createNewFile();
+            fo = new FileOutputStream(photoFile);
+            fo.write(bytes.toByteArray());
+            fo.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+         Glide.with(this).load(photoFile).into(profile_image);
+        profile_image.setVisibility(View.VISIBLE);
+    }
+
+    @SuppressWarnings("deprecation")
+    private void onSelectFromGalleryResult(Intent data) {
+        if (data != null) {
+            try {
+                Bitmap thumbnail = MediaStore.Images.Media.getBitmap(getContentResolver(), data.getData());
+                ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                thumbnail.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+
+                photoFile = new File(Environment.getExternalStorageDirectory(), System.currentTimeMillis() + ".jpg");
+                FileOutputStream fo;
+                photoFile.createNewFile();
+                fo = new FileOutputStream(photoFile);
+                fo.write(bytes.toByteArray());
+                fo.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        Glide.with(this).load(photoFile).into(profile_image);
+        acceptImage.setVisibility(View.VISIBLE);
+    }
+
+
+    public boolean checkStoragePermission(final AppCompatActivity activity) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) return true;
+
+        if (ContextCompat.checkSelfPermission(activity.getBaseContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }
+
+        if (ContextCompat.checkSelfPermission(activity.getBaseContext(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+            alertBuilder.setCancelable(true);
+            alertBuilder.setTitle("Permission Required");
+            alertBuilder.setMessage("Permision to Read/Write to External storage is required to proceed");
+            alertBuilder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_EXTERNAL_STORAGE_PERMISSIONS);
+                }
+            });
+            AlertDialog alert = alertBuilder.create();
+            alert.show();
+        } else {
+            ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_EXTERNAL_STORAGE_PERMISSIONS);
+        }
+        return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_EXTERNAL_STORAGE_PERMISSIONS:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (choosenAction == 0) photoCameraIntent();
+                    else photoGalleryIntent();
+                }
+                break;
+        }
+    }
+
+    private void photoCameraIntent() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, IMAGE_CAMERA_REQUEST);
+    }
+
+    private void photoGalleryIntent() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+//        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, getString(R.string.select_picture_title)), IMAGE_GALLERY_REQUEST);
+    }
+
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == IMAGE_GALLERY_REQUEST) {
+                if (data != null)
+                    onSelectFromGalleryResult(data);
+            } else if (requestCode == IMAGE_CAMERA_REQUEST) {
+                if (data != null)
+                    onCaptureImageResult(data);
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbinder.unbind();
     }
 }
